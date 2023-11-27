3a563114d13486e5785c3a4be075d29e55f19251
Inokus <inokus@outlook.com>
2023-11-27 18:02:29 +0200

    Add interactive_learning_tool.py

---
+++ interactive_learning_tool.py
@@ -0,0 +1,100 @@
+from classes.assessment import Practice, Test
+from classes.file_handler import FileHandler
+from classes.question_handler import AddingQuestions, DisableEnableQuestions
+from classes.statistics_viewing import StatisticsViewing
+from utils.utils import valid_name, get_user_input
+
+
+def print_menu(condition):
+    # If condition is not met print practice and test in gray color
+    practice = ("\033[90m4 Practice\033[0m") if not condition else "4 Practice"
+    test = ("\033[90m5 Test\033[0m") if not condition else "5 Test"
+
+    print(
+        "\n1 Add questions\n"
+        "--------------------------------------------\n"
+        "2 View statistics\n"
+        "--------------------------------------------\n"
+        "3 Disable/Enable questions\n"
+        "--------------------------------------------\n"
+        f"{practice}\n"
+        "--------------------------------------------\n"
+        f"{test}\n"
+        "--------------------------------------------\n"
+        "6 Change profile\n"
+        "--------------------------------------------\n"
+        "7 Exit\n"
+    )
+
+
+def main():
+    while True:
+        while True:
+            name = get_user_input("Enter the name of your profile: ", "sl")
+            if valid_name(name):
+                break
+
+        # Create file handler for current user and ensure that all files/folders are set up
+        file_handler = FileHandler(name)
+        if not file_handler.paths_exists():
+            file_handler.create_paths()
+        # Ensure that in case some other user added questions they're also added to current user stats
+        file_handler.write_user_stats()
+
+        # Make sure that at least 5 questions are added before enabling practice and test modes
+        enable_practice_and_test = False
+        questions_count = file_handler.get_length(file_handler.questions_file_name)
+        if questions_count >= 5:
+            enable_practice_and_test = True
+
+        print_menu(enable_practice_and_test)
+        while True:
+            mode = input("Type [1-7] to select: ")
+            match mode:
+                case "1":
+                    adding_questions = AddingQuestions(file_handler)
+                    adding_questions.add_questions()
+                    file_handler.write_user_stats()
+                    questions_count = file_handler.get_length(
+                        file_handler.questions_file_name
+                    )
+                    if questions_count >= 5:
+                        enable_practice_and_test = True
+                    print_menu(enable_practice_and_test)
+                case "2":
+                    statistics_viewing = StatisticsViewing(file_handler)
+                    statistics_viewing.view_statistics()
+                    print_menu(enable_practice_and_test)
+                case "3":
+                    disable_enable_questions = DisableEnableQuestions(file_handler)
+                    disable_enable_questions.change_status()
+                    print_menu(enable_practice_and_test)
+                case "4":
+                    if enable_practice_and_test:
+                        practice = Practice(file_handler)
+                        practice.start_practice()
+                        print_menu(enable_practice_and_test)
+                    else:
+                        print(
+                            f"This mode is currently disabled, add {5 - questions_count} or more questions for access"
+                        )
+                case "5":
+                    if enable_practice_and_test:
+                        test = Test(file_handler)
+                        test.start_test()
+                        print_menu(enable_practice_and_test)
+                    else:
+                        print(
+                            f"This mode is currently disabled, add {5 - questions_count} or more questions for access"
+                        )
+                case "6":
+                    print()
+                    break
+                case "7":
+                    return
+                case _:
+                    print("Invalid input")
+
+
+if __name__ == "__main__":
+    main()
